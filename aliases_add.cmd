d22a457447b86c9e0e1b53de0368313b507c6bbc
Руслан Жданов <zhdanovri@silverbulleters.org>
2020-11-20 16:12:50 +0600

    исправил опечатки и убрал лишнее

--- aliases_add.cmd
+++ aliases_add.cmd
@@ -1,5 +1,5 @@
 @echo off 
 
-set aliasfile=%~dp0aliasfile.doskey
+set aliasfile=%~dp0aliasesfile.doskey
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v DEFAULT_NAMESPACE /D default
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v Autorun /d "doskey /macrofile=\"%aliasfile%"" /f
